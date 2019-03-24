e9fc7c8ff89f57c5b0971dbb303dfe85edb4a18d
PatrykPilecki <patryk.pilecki@onet.eu>
2019-03-24 22:59:17 +0100

    strona nieruchomosci

---
+++ index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ochota</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.6.1/gridlex.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <?php include('form_process.php'); ?>
+<link rel="stylesheet" href="form.css" type="text/css">
+</head>
+
+<body>
+
+
+    <div class="pojemnik">
+<img class="log2" src="logo.png " alt="tlo " style="max-width:30%;position:absolute;left:10px;">
+
+        <div class="grid-2" id="top1" style="background-image: url( 'headpg1.png' );height:795px;background-size:cover;">
+            <div class="col-5"></div>
+            <div class="col-7_sm-12" style="background-image: url( 'warstwa-5.png' );height:390px;margin-top:280px;">
+                <h1 style="
+                    color: #ffffff;
+                    font-family: 'Playfair Display';
+                    font-size: 45px;
+                    font-weight: 700;text-align:left;margin-left:50px;margin-top:50px;line-height:44px;
+                    ">Zamieszkaj na Ochocie</h1>
+ <div class="col-6"><img src="linia.png " alt="tlo " style="max-width:600px;float:left;margin-top:20px;margin-left:50px;"></div>
+<p class="toptxt" style="height: 128px;
+color: #b7bdc9;
+font-family: 'Open Sans', sans-serif;
+font-size: 14px;
+font-weight: 300;
+line-height: 22px;text-align:justify;margin-left:50px;margin-top:15px;max-width:500px;">  Stara Ochota to jedna z najstarszych i najbardziej znanych dzielnic Warszawy. To tętniące życiem serce miasta i miejsce, w którym można poczuć prawdziwy klimat stolicy. Zlokalizowana w samym centrum Starej Ochoty nowoczesna, prestiżowa inwestycja to idealna propozycja dla najbardziej wymagających. To tu w jednym miejscu harmonijnie łączą się luksus, nowoczesność i tradycja.
+ </p>
+            </div>
+        </div>
+
+        <div class="grid" id="top2">
+            <div class="col-6_sm-12" id="dwie" style="background-image: url( 'slice-8.png' );background-repeat:no-repeat;background-size:cover;background-position:center;z-index:1;height: 400px;">
+         
+
+    
+<h1 style="max-width: 515px;
+height: 46px;
+color: #4c4b46;
+font-family: 'Playfair Display';
+font-size: 45px;
+font-weight: 700;
+margin-top:130px;margin-right:20px;float:right;">Tradycja i nowoczesność</h1>
+
+<p class="story"style="z-index:2;color: #4c4b46; font-family: 'Open Sans', sans-serif;font-weight: 400;text-align:justify;margin:40px;max-width: 463px;height: 133px;color: #4c4b46;font-weight: 400;float:right;line-height:16px;">
+Historia dzielnicy Ochota sięga XVIII w., kiedy istniała tu wieś Wielka Wola. Bodźcem sprzyjającym rozwojowi było otwarcie w latach 40. XIX wieku kolei Warszawsko - Wiedeńskiej. Prawdziwy boom budowlany na Ochocie nastąpił jednak w okresie
+                    dwudziestolecia międzywojennego. Od tej chwili Ochota jest jedną z najbardziej pożądanych dzielnic mieszkaniowych Warszawy. Wśród jej największych walorów wymieniane są: dogodna lokalizacja, bliskość terenów zielonych oraz świetne
+                    skomunikowanie ze ścisłym centrum stolicy.</p>
+            </div>
+            <div class="col-6_sm-12" id="dwie" style="height:400px;">
+<iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://my.ctrlq.org/maps/#roadmap|14|52.2103136606511|20.97160599999995"></iframe>
+</div>
+</div>
+        <div class="grid" id="top3">
+
+             <div class="col-4_sm-12" style="background-image: url( '4.png' );background-repeat:no-repeat;background-size:cover;background-position:center;z-index:1;height: 400px;">
+<div class="cont" style="float:right;">
+                <h1 style="
+                    height: 24px;
+                    color: #ffffff;
+                    font-family: ' Playfair Display ';
+                    font-size: 24px;
+                    font-weight: 700;max-width: 300px;
+                    height: 61px;
+                    border: 1px solid #967b50;margin-top:20px;margin-left:15px;margin-top:50px;padding-top:20px;"> Doskonała lokalizacja</h1>
+
+                <p style="z-index:2;
+                    height: 132px;
+                    color: #ffffff;
+                    font-family:  'Playfair Display ';
+                    font-size: 16px;
+                    font-weight: 600;margin:40px;max-width:270px;line-height:20px;">
+                    Z położonej w sąsiedztwie terenów zielonych kameralnej, prestiżowej inwestycja do ścisłego centrum Warszawy można dotrzeć w niecałe 15 minut, a liczne tereny rekreacyjne w sąsiedztwie zachęcają do aktywnego wypoczynku.
+                </p>
+</div>
+
+            </div>
+            <div class="col-4_sm-12" style="background-image: url( '5.png' );background-repeat:no-repeat;background-size:cover;background-position:center;z-index:1;height: 400px;">
+                <h1 style="
+                    height: 24px;
+                    color: #ffffff;
+                    font-family: ' Playfair Display ';
+                    font-size: 24px;
+                    font-weight: 700;max-width: 200px;
+                    height: 61px;
+                    border: 1px solid #967b50;margin-left:auto;margin-right:auto;margin-top:50px;padding-top:20px;"> Luksus</h1>
+
+                <p style="z-index:2;
+                  
+                    color: #ffffff;
+                    font-family: 'Playfair Display ';
+                    font-size: 16px;
+                    font-weight: 600;max-width:250px;margin-left:auto;margin-right:auto;line-height:20px;margin-top:30px;">
+                    Wyjątkowa, świetnie wkomponowana w tkankę miejską architektura idzie w parze z wysokiej jakości materiałami wykończeniowymi, dzięki czemu przyszłych mieszkańców otaczać będzie prawdziwie luksusowy klimat.
+                </p>
+
+            </div>
+
+            <div class="col-4_sm-12" style="background-image: url( '8.png' );background-repeat:no-repeat;background-size:cover;background-position:center;z-index:1;height: 400px;">
+                <h1 style="
+                    height: 24px;
+                    color: #ffffff;
+                    font-family: ' Playfair Display ';
+                    font-size: 24px;
+                    font-weight: 700;max-width: 300px;
+                    height: 61px;
+                    border: 1px solid #967b50;margin-top:50px;margin-left:50px;padding-top:20px;"> Nowoczesne rozwiązania</h1>
+
+
+                <p style="z-index:2;
+                height: 132px;
+                color: #ffffff;
+                font-family:'Playfair Display ';
+                font-size: 16px;
+                font-weight: 600;margin:40px;max-width:290px;line-height:20px;">
+                    W apartamentach do dyspozycji mieszkańców będą wszelkiego rodzaju udogodnienia, dostarczane z wykorzystaniem najnowszych technologii, w tym nowoczesny system zarządzania typu „smart home”.
+                </p>
+
+            </div>
+        </div>
+
+        <div class="grid" id="top4" style="   text-align: center; height: 400px;
+        background-color: #6a6a6a;
+        background-image: linear-gradient(to right, rgba(7, 39, 72, 0.66) 0%, rgba(29, 73, 129, 0.66) 48%, rgba(7, 39, 72, 0.66) 100%);">
+            <div class="col-12">
+                <h1 style=" height: 17px;
+                color: #ffffff;
+                font-family: ' Playfair Display ';
+                font-size: 35px;
+                font-weight: 700;margin-top:50px;max-width:900px;text-align:center;margin-left: auto; margin-right: auto;    line-height: 44px;  ">Butikowa inwestycja w doskonałej lokalizacji</h1>
+
+
+           </div>
+
+
+            <p style="
+            height: 200px;
+            color: #dddeff;
+            font-family: 'Open Sans', sans-serif;
+            font-size: 14px;
+            font-weight: 200;text-align:center;max-width:919px;margin-left: auto; margin-right: auto;line-height:20px;">Nowoczesna, kameralna inwestycja, która powstaje w centrum Ochoty to propozycja dla najbardziej wymagających Klientów, którzy nie godzą się na kompromisowe rozwiązania. To idealna propozycja dla osób chcących w wygodny sposób łączyć życie
+                w centrum miasta ze spokojem i kameralną atmosferą mieszkania w nowoczesnej, butikowej inwestycji.</br></br></br><a href="#top6" class="a" style="text-decoration: none;
+                color: #ffffff;
+                font-family: 'Open Sans';
+                font-size: 14px;
+                font-weight: 300;
+                
+                margin-left:auto;margin-right:auto;
+                letter-spacing: 2.1px;
+                width: 320px;
+                height: 70px;
+                border: 1px solid #967b50;padding:10px;">DOWIEDZ SIĘ WIĘCEJ!</a></p>
+
+ 
+        
+
+
+</div>
+
+        <div class="grid" id="top5">
+
+            <div class="col-6_sm-12" id="top5-a" style="height:400px;background-image: url( '17.png' );width:100%;background-repeat:no-repeat;background-size:cover;background-position:center;z-index:1;">
+<p style="color: #967b50;
+font-family: 'Open Sans - Semibold';
+font-size: 14px;
+font-weight: 400;
+margin-top:140px;
+margin-left:130px;
+letter-spacing: 2.1px;">PRZESTRZEŃ I ELEGANCJA</p>
+                <h1 style="margin-right:100px;
+                    height: 137px;
+                    color: #4c4b46;
+                    font-family: ' Playfair Display ';
+                    font-size: 55px;
+                    font-weight: 600;max-width: 292px;float:right;"> Galeria</h1>
+
+            </div>
+
+            <div class="col-6_sm-12" id="top5-b" style="">
+
+                <div class="w3-content w3-display-container" style="">
+                    <img class="mySlides" src="a.png" style="width:100%;height:400px;">
+                    <img class="mySlides" src="b.png" style="width:100%;height:400px;">
+                    <img class="mySlides" src="3z.jpg" style="width:100%;height:400px;">
+                    <img class="mySlides" src="d.png" style="width:100%;height:400px;">
+                    <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+                        <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)" style="cursor:pointer;">&#10094;</div>
+                        <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)" style="cursor:pointer;">&#10095;</div>
+                        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)" style="cursor:pointer;"></span>
+                        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)" style="cursor:pointer;"></span>
+                        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)" style="cursor:pointer;"></span>
+                        <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(4)" style="cursor:pointer;"></span>
+                    </div>
+                </div>
+
+                <script>
+                    var slideIndex = 1;
+                    showDivs(slideIndex);
+
+                    function plusDivs(n) {
+                        showDivs(slideIndex += n);
+                    }
+
+                    function currentDiv(n) {
+                        showDivs(slideIndex = n);
+                    }
+
+                    function showDivs(n) {
+                        var i;
+                        var x = document.getElementsByClassName("mySlides");
+                        var dots = document.getElementsByClassName("demo");
+                        if (n > x.length) {
+                            slideIndex = 1
+                        }
+                        if (n < 1) {
+                            slideIndex = x.length
+                        }
+                        for (i = 0; i < x.length; i++) {
+                            x[i].style.display = "none";
+                        }
+                        for (i = 0; i < dots.length; i++) {
+                            dots[i].className = dots[i].className.replace(" w3-white", "");
+                        }
+                        x[slideIndex - 1].style.display = "block";
+                        dots[slideIndex - 1].className += " w3-white";
+                    }
+                </script>
+
+
+            </div>
+
+
+        </div>
+
+
+        <div class="grid" id="top6" >
+            <div class="col-6_sm-12" id="top6-a" style="background-image: url( '6.png' );background-repeat:no-repeat;background-size:cover;background-position:center;z-index:1;height:350px;">
+               <div class="kontact" style="float:right;margin-top:15px;margin-right:15px;">
+ <h1 style="
+                    height: 30px;
+                    color: #ffffff;
+                    font-family: ' Playfair Display ';
+                    font-size: 55px;
+                    font-weight: 700;margin-right:90px;margin-top:35px;">Kontakt</h1>
+
+
+                <p style="margin-top:50px;margin-right:150px;
+                        height: 13px;
+                        color: #ffffff;
+                        font-family: 'Open Sans', sans-serif;
+                        font-size: 16px;
+                        font-weight: 300;"><img src="phone-call.png" alt="tlo" style="max-width:100%;"> +48 783 939 343</p>
+
+
+                <p style="margin-top:30px;margin-right:35px;
+                            height: 13px;
+                            color: #ffffff;
+                           font-family: 'Open Sans', sans-serif;
+                            font-size: 16px;
+                            font-weight: 300;"><img src="envelope.png" alt="tlo" style="max-width:100%;"> kontakt@poland-sothebysrealty.pl</p>
+
+
+
+                <p style="margin-top:30px;margin-right:95px;
+                            height: 13px;
+                            color: #ffffff;
+                            font-family: 'Open Sans', sans-serif;
+                            font-size: 16px;
+                            font-weight: 300;"><img src="house.png" alt="tlo" style="max-width:100%;"> poland-sothebysrealty.pl</p>
+</div>
+            </div>
+
+
+
+
+            <div class="col-6_sm-12" id="top6-b" style=""> 
+<h3 style="font-family: 'Playfair Display';color:#967b50;max-width:400px;font-size:22px;padding-top:10px;margin-left:30px;">Wypełnij formularz i dowiedz się więcej!<h3>
+            
+            <form id="contact" action="<?= htmlspecialchars($_SERVER[" PHP_SELF "]) ?>" method="post">
+
+        <fieldset>
+            <input style="color: #2e2e2e;
+font-family: 'Playfair Display';
+font-size: 16px;
+font-weight: 700;margin-left:30px;" placeholder="Imię i Nazwisko" type="text" name="name" value="<?= $name ?>" tabindex="1" autofocus>
+            <span class="error"><?= $name_error ?></span>
+        </fieldset>
+        <fieldset>
+            <input style="color: #2e2e2e;
+font-family: 'Playfair Display';
+font-size: 16px;
+font-weight: 700;margin-left:30px;"placeholder="Email" type="text" name="email" value="<?= $email ?>" tabindex="2">
+            <span class="error"><?= $email_error ?></span>
+        </fieldset>
+        <fieldset>
+            <input style="color: #2e2e2e;
+font-family: 'Playfair Display';
+font-size: 16px;
+font-weight: 700;margin-left:30px;" placeholder="Telefon" type="text" name="phone" value="<?= $phone ?>" tabindex="3">
+            <span class="error"><?= $phone_error ?></span>
+        </fieldset>
+
+
+        <fieldset >
+<p style="float:left;font-size:10px;color: #2e2e2e;
+font-family: 'Playfair Display';
+font-size: 9px;
+font-weight: 700;margin-left:30px;line-height:10px;">
+<input type="checkbox" > Wyrażam zgodę na przetwarzanie moich danych osobowych, zgodnie z Ustawą z dnia 29 sierpnia 1997 r. o ochronie danych osobowych (Dz. U. 1997 r. Nr 133, poz. 883 z późn. zm.
+</p>
+</fieldset>
+
+        <fieldset>
+
+            <button  name="submit" type="submit" id="contact-submit" data-submit="...Sending">Chcę poznać więcej szczegołów!</button>
+        </fieldset>
+        <div class="success">
+            <?= $success ?>
+        </div>
+    </form>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+</body>
